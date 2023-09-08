02ec9bc175ab502406d64b07e2c181f6e55692f7
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-09-08 14:28:27 +0200

    Aggiungere i file di progetto.

---
+++ csharp-oop-shop/Product.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop
+{
+    public class Product
+    {
+
+        //attributes
+        private double serialNumber;
+        private string name;
+        private string description = "Nessuna descrizione disponibile";
+        private float price;
+        private float vat;
+
+        //construcor
+
+        public Product(string productName, float productPrice, float productVat )
+        {
+            this.Name = productName;
+            this.Price = productPrice;
+            this.Vat = productVat;
+            GenerateSerialNumber();
+            
+        }
+
+        //PROPRETIES
+
+
+        //serial number
+        public double SerialNumber
+        {
+            get {
+                return this.serialNumber;
+            }
+        }
+
+
+        //name
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            set { this.name = value; }
+        }
+
+
+        //description
+        public string Description
+        {
+            get
+            {
+                return this.description;
+            }
+            set 
+            {
+                if (value.Length < 255)
+                    this.description = value;
+                else
+                    Console.WriteLine("Descrizione troppo lunga");
+            }
+        }
+
+        //serial number
+        public float Price
+        {
+            get
+            {
+                return this.price;
+            }
+
+            set
+            {
+                if (value > 0)
+                    this.price = value;
+                else
+                    Console.WriteLine("Indicare un prezzo");
+            }
+        }
+
+        //vat
+        public float Vat
+        {
+            get
+            {
+                return this.vat;
+            }
+
+            set
+            {
+                if (value > 0)
+                    this.vat = value;
+                else
+                    Console.WriteLine("Inserire IVA del prodotto");
+            }
+        }
+
+        //GETTER
+
+        public float getIvaTotalPrice()
+        {
+            float totalPrice = 0;
+            totalPrice = this.price + ((this.price *  this.vat) / 100);
+            return MathF.Round(totalPrice, 2);
+
+        }
+
+        public string getIdentificationName()
+        {
+            string fullName = $"{this.name} (S/N {this.serialNumber})";
+            return fullName;
+        }
+
+
+
+        //logical function
+        private void GenerateSerialNumber()
+        {
+            string serialString = "";
+            Random rnd = new Random();
+            for(int i = 1; i < 14;  i++)
+            {
+                serialString += $"{rnd.Next(10)}";
+            }
+            this.serialNumber = double.Parse(serialString);
+        }
+    }
+}
---
+++ csharp-oop-shop/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace csharp_oop_shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product latteBerna = new Product("Latte Berna", 1.20f, 15f);
+
+            Console.WriteLine();
+            Console.WriteLine($" Benvenuto alla pagina del prodotto {latteBerna.Name}");
+            Console.WriteLine($" Numero seriale: {latteBerna.SerialNumber}");
+            Console.WriteLine($" Nome univoco: {latteBerna.getIdentificationName()}");
+            Console.WriteLine($" Descrizione del prodotto: {latteBerna.Description}");
+            Console.WriteLine($" Prezzo compresa IVA {latteBerna.Vat}%: {latteBerna.getIvaTotalPrice()} || Prezzo originale: {latteBerna.Price} ");
+
+        }
+    }
+}
